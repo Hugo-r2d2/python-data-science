9203fb86a7e6a1ce927608a623349c9399e5c2c1
Hugo Camelo dos Santos <ihugocs@gmail.com>
2024-12-27 22:06:59 -0400

    explicando

--- aula.py
+++ aula.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 x = [1, 2]
 y = [2, 3]
 
-plt.plot(x, y)
+plt.plot(x, y) # Plot é uma função para criar os gráfios de linha, traça pontos em um gráfico conectando com linhas
 
-plt.show()
+plt.show() # show é usado para exibir o gráfico 
